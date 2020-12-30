8be932c5ac756a98db951b6f6ea50a114d3c2cbe
suzuki-navi <suzuki.navi.dev@gmail.com>
2020-12-30 11:24:29 +0900

    Twitterに自動ツイート

---
+++ tweet/tweet.py
@@ -0,0 +1,71 @@
+import datetime
+import os
+import boto3
+import twitter
+
+TIME_UNIT = 5
+
+s3_bucket = os.environ["DATA_S3_BUCKET"]
+s3_key    = os.environ["DATA_S3_KEY"]
+twitter_consumer_key    = os.environ["TWITTER_CONSUMER_KEY"]
+twitter_consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+twitter_token           = os.environ["TWITTER_TOKEN"]
+twitter_token_secret    = os.environ["TWITTER_TOKEN_SECRET"]
+
+def main(event, context):
+    now = datetime.datetime.utcnow()
+    for record in readDataLines(now):
+        message = record[1]
+        print(f"tweet: {message}")
+        tweet(message)
+        #postSlack(message)
+
+def readDataLines(now):
+    lines1 = readS3Object().split(sep='\n')
+    lines2 = [parseDataLine(line) for line in lines1]
+    lines3 = [line for line in lines2 if line != None]
+    print(lines3)
+    lines4 = [line for line in lines3 if isTimeMatch(line, now)]
+    return lines4
+
+def isTimeMatch(record, now):
+    dt = record[0]
+    m0 = int(now.minute / TIME_UNIT) * TIME_UNIT
+    if dt.year == now.year and dt.month == now.month and dt.day == now.day and dt.hour == now.hour:
+        m1 = int(dt.minute / TIME_UNIT) * TIME_UNIT
+        if m1 == m0:
+            return True
+    return False
+
+def parseDataLine(line):
+    p = line.find('#')
+    if (p >= 0):
+        line = line[0:p]
+    line = line.strip()
+    cols = line.split(sep=' ', maxsplit=1)
+    try:
+        dt = datetime.datetime.fromisoformat(cols[0] + "+09:00").astimezone(datetime.timezone.utc)
+        msg = cols[1].strip()
+    except:
+        return None
+    return [dt, msg]
+
+def readS3Object():
+    session = boto3.session.Session()
+    s3_client = session.client("s3")
+    res = s3_client.get_object(
+        Bucket = s3_bucket,
+        Key = s3_key,
+    )
+    return res['Body'].read().decode("utf-8")
+
+def tweet(message):
+    auth = twitter.OAuth(
+        consumer_key    = twitter_consumer_key,
+        consumer_secret = twitter_consumer_secret,
+        token           = twitter_token,
+        token_secret    = twitter_token_secret,
+       )
+    client = twitter.Twitter(auth = auth)
+    client.statuses.update(status = message)
+
