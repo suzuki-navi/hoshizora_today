0c46f84ac80c540e6545f167d4a8506776fddd02
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-05-15 10:06:33 +0900

    refactoring

---
+++ calc/mathlib.scala
@@ -0,0 +1,303 @@
+
+object MathLib {
+
+  def circleAdd(a: Double, b: Double): Double = {
+    val d = a + b;
+    if (d >= 3 * PI) {
+      d - 2 * PI2;
+    } else if (d >= PI) {
+      d - PI2;
+    } else if (d < -3 * PI) {
+      d + 2 * PI2;
+    } else if (d < -PI) {
+      d + PI2;
+    } else {
+      d;
+    }
+  }
+
+  // 0以上になるインデックスを探す
+  def binarySearchBy[A](seq: IndexedSeq[A])(f: A => Double): Int = {
+    @scala.annotation.tailrec
+    def sub(start: Int, end: Int): Int = {
+      if (end - start == 1) {
+        start;
+      } else if (end - start == 2) {
+        if (f(seq(start)) >= 0.0) {
+          start;
+        } else {
+          start + 1;
+        }
+      } else {
+        val mid = (end + start) / 2;
+        val midf = f(seq(mid));
+        if (midf <= 0.0) {
+          sub(mid, end);
+        } else {
+          sub(start, mid + 1);
+        }
+      }
+    }
+    sub(0, seq.size);
+  }
+
+  // 2つ目の返値 +1: 最大値
+  // 2つ目の返値 -1: 最小値
+  def findMaxMinListContinuous(start: Double, end: Double, step: Double, partitionCount: Int)(f: Double => Double): IndexedSeq[(Double, Int)] = {
+    findMaxMinListContinuous(0, ((end - start) * partitionCount).toInt, (step * partitionCount).toInt) { x =>
+      f(x / partitionCount + start);
+    }.map { case (x, p) =>
+      (x / partitionCount + start, p);
+    }
+  }
+
+  // 2つ目の返値 +1: 最大値
+  // 2つ目の返値 -1: 最小値
+  def findMaxMinListContinuous(start: Int, end: Int, step: Int)(f: Double => Double): IndexedSeq[(Double, Int)] = {
+    findMaxMinListDiscrete(start, end, step)(x => f(x.toDouble)).map { case (x, p) =>
+      (x + findExtremum(f(x - 1), f(x), f(x + 1)), p);
+    }
+  }
+
+  // 返値 0: 増加中
+  // 返値 1: 最大値
+  // 返値 2: 減少中
+  // 返値 3: 最小値
+  def getMaxMinUpDownFlagListDiscrete(start: Int, end: Int, step: Int)(f: Int => Double): IndexedSeq[Int] = {
+    val maxMinList = findMaxMinListDiscrete(start, end, step)(f);
+    if (maxMinList.size == 0) {
+      if (f(start) > f(end)) {
+        IndexedSeq.fill(end - start)(2);
+      } else {
+        IndexedSeq.fill(end - start)(0);
+      }
+    } else {
+      var listIdx: Int = 0;
+      var nextIdx: Int = maxMinList(0)._1;
+      var flag: Int = maxMinList(0)._2;
+      (start until end).map { i =>
+        if (i == nextIdx) {
+          val value = 2 - flag;
+          listIdx += 1;
+          if (listIdx == maxMinList.size) {
+            nextIdx = end;
+            flag = - flag;
+          } else {
+            nextIdx = maxMinList(listIdx)._1;
+            flag = maxMinList(listIdx)._2;
+          }
+          value;
+        } else {
+          1 - flag;
+        }
+      }
+    }
+  }
+
+  // 2つ目の返値 +1: 最大値
+  // 2つ目の返値 -1: 最小値
+  def findMaxMinListDiscrete(start: Int, end: Int, step: Int)(f: Int => Double): IndexedSeq[(Int, Int)] = {
+    val fm = { x: Int => -f(x) }
+    var result: List[(Int, Int)] = Nil;
+    var s: Int = start;
+    var sv: Double = f(s);
+    var t: Int = s + step;
+    if (t >= end) {
+      return IndexedSeq.empty;
+    }
+    var tv: Double = f(t);
+    var u: Int = t + step;
+    while (u < end) {
+      val uv = f(u);
+      if (sv < tv && tv > uv) {
+        val max = findMaxDiscrete(s, t, u, sv, tv, uv)(f);
+        result = (max, +1) :: result;
+      } else if (sv > tv && tv < uv) {
+        val min = findMaxDiscrete(s, t, u, -sv, -tv, -uv)(fm);
+        result = (min, -1) :: result;
+      }
+      s = t;
+      sv = tv;
+      t = u;
+      tv = uv;
+      u = u + step;
+    }
+    result.reverse.toIndexedSeq;
+  }
+
+  // 2つ目の返値 0: 0
+  // 2つ目の返値 1: 最大値
+  // 2つ目の返値 2: 0
+  // 2つ目の返値 3: 最小値
+  def findMaxMinCrossingListContinuous(start: Double, end: Double, step: Double, partitionCount: Int)(f: Double => Double): IndexedSeq[(Double, Int)] = {
+    findMaxMinCrossingListContinuous(0, ((end - start) * partitionCount).toInt, (step * partitionCount).toInt) { x =>
+      f(x / partitionCount + start);
+    }.map { case (x, p) =>
+      (x / partitionCount + start, p);
+    }
+  }
+
+  // 2つ目の返値 0: 0
+  // 2つ目の返値 1: 最大値
+  // 2つ目の返値 2: 0
+  // 2つ目の返値 3: 最小値
+  def findMaxMinCrossingListContinuous(start: Int, end: Int, step: Int)(f: Double => Double): IndexedSeq[(Double, Int)] = {
+    findMaxMinCrossingListDiscrete(start, end, step)(x => f(x.toDouble)).map { case (x, p) =>
+      if (p % 2 != 0) {
+        (x + findExtremum(f(x - 1), f(x), f(x + 1)), p);
+      } else if (p == 0) {
+        (x + findCrossing(f(x), f(x + 1)), p);
+      } else {
+        (x + findCrossing(-f(x), -f(x + 1)), p);
+      }
+    }
+  }
+
+  // 2つ目の返値 0: 0
+  // 2つ目の返値 1: 最大値
+  // 2つ目の返値 2: 0
+  // 2つ目の返値 3: 最小値
+  def findMaxMinCrossingListDiscrete(start: Int, end: Int, step: Int)(f: Int => Double): IndexedSeq[(Int, Int)] = {
+    val fm = { x: Int => -f(x) }
+    var result: List[(Int, Int)] = Nil;
+    var s: Int = start;
+    var sv: Double = f(s);
+    var t: Int = s + step;
+    if (t >= end) {
+      return IndexedSeq.empty;
+    }
+    var tv: Double = f(t);
+    if (sv <= 0.0 && tv > 0.0) {
+      val cr = findCrossingDiscrete(s, t, sv, tv)(f);
+      result = (cr, 0) :: result;
+    } else if (sv >= 0.0 && tv < 0.0) {
+      val cr = findCrossingDiscrete(s, t, -sv, -tv)(fm);
+      result = (cr, 2) :: result;
+    }
+    var u: Int = t + step;
+    while (u < end) {
+      val uv = f(u);
+      if (sv < tv && tv > uv) {
+        val max = findMaxDiscrete(s, t, u, sv, tv, uv)(f);
+        result = (max, 1) :: result;
+      } else if (sv > tv && tv < uv) {
+        val min = findMaxDiscrete(s, t, u, -sv, -tv, -uv)(x => -f(x));
+        result = (min, 3) :: result;
+      }
+      if (tv <= 0.0 && uv > 0.0) {
+        val cr = findCrossingDiscrete(t, u, tv, uv)(f);
+        result = (cr, 0) :: result;
+      } else if (tv >= 0.0 && uv < 0.0) {
+        val cr = findCrossingDiscrete(t, u, -tv, -uv)(fm);
+        result = (cr, 2) :: result;
+      }
+      s = t;
+      sv = tv;
+      t = u;
+      tv = uv;
+      u = u + step;
+    }
+    result.reverse.toIndexedSeq;
+  }
+
+  def findCyclicPhaseListContinuous(cycle: Int, start: Double, end: Double, step: Double, partitionCount: Int)(f: Double => Double): IndexedSeq[(Double, Int)] = {
+    findCyclicPhaseListContinuous(cycle, 0, ((end - start) * partitionCount).toInt, (step * partitionCount).toInt) { x =>
+      f(x / partitionCount + start);
+    }.map { case (x, p) =>
+      (x / partitionCount + start, p);
+    }
+  }
+
+  def findCyclicPhaseListContinuous(cycle: Int, start: Int, end: Int, step: Int)(f: Double => Double): IndexedSeq[(Double, Int)] = {
+    val cycle_r = PI2R * cycle;
+    val cycle2 = cycle * 0.5;
+    val fs = (0 until cycle).map { i =>
+      { x: Int =>
+        val y = f(x) * cycle_r - i;
+        if (y >= cycle2) {
+          y - cycle;
+        } else {
+          y;
+        }
+      }
+    }
+    val fs0 = fs(0);
+    var result: List[(Double, Int)] = Nil;
+    var prevX: Int = start;
+    var nextI: Int = (f(prevX) * cycle_r).toInt + 1;
+    if (nextI == cycle) nextI = 0;
+    var fsi = fs(nextI);
+    var prevValue: Double = fsi(prevX)
+    while (prevX + 1 < end) {
+      val x = (prevX + step) min (end - 1);
+      val value = fsi(x);
+      if (value > 0.0) {
+        val t = findCrossingDiscrete(prevX, x, prevValue, value)(fsi);
+        val t2 = t + findCrossing(fsi(t), fsi(t + 1));
+        result = (t2, nextI) :: result;
+        nextI += 1;
+        if (nextI == cycle) nextI = 0;
+        fsi = fs(nextI);
+        prevValue = fsi(x);
+      } else {
+        prevValue = value;
+      }
+      prevX = x;
+    }
+    return result.reverse.toIndexedSeq;
+  }
+
+  @scala.annotation.tailrec
+  private def findMaxDiscrete(s: Int, t: Int, u: Int, sv: Double, tv: Double, uv: Double)(f: Int => Double): Int = {
+    if (s + 1 == t && t + 1 == u) {
+      t;
+    } else if (s + u >= 2 * t) {
+      val c = (u + t) / 2;
+      val cv = f(c);
+      if (cv >= tv) {
+        findMaxDiscrete(t, c, u, tv, cv, uv)(f);
+      } else {
+        findMaxDiscrete(s, t, c, sv, tv, cv)(f);
+      }
+    } else {
+      val c = (s + t) / 2;
+      val cv = f(c);
+      if (cv > tv) {
+        findMaxDiscrete(s, c, t, sv, cv, tv)(f);
+      } else {
+        findMaxDiscrete(c, t, u, cv, tv, uv)(f);
+      }
+    }
+  }
+
+  @scala.annotation.tailrec
+  private def findCrossingDiscrete(s: Int, t: Int, sv: Double, tv: Double)(f: Int => Double): Int = {
+    if (s + 1 == t) {
+      s;
+    } else {
+      val c = (s + t) / 2;
+      val cv = f(c);
+      if (cv > 0.0) {
+        findCrossingDiscrete(s, c, sv, cv)(f);
+      } else {
+        findCrossingDiscrete(c, t, cv, tv)(f);
+      }
+    }
+  }
+
+  private def findExtremum(sv: Double, tv: Double, uv: Double): Double = {
+    val a: Double = (uv + sv) * 0.5 - tv;
+    val b: Double = (uv - sv) * 0.5;
+    -0.5 * b / a;
+  }
+
+  private def findCrossing(sv: Double, tv: Double): Double = {
+    -sv / (tv - sv);
+  }
+
+  private val PI = Math.PI;
+  private val PI2 = Math.PI * 2.0;
+  private val PI2R = 1.0 / PI2;
+
+}
+
---
+++ calc/timelib.scala
@@ -0,0 +1,154 @@
+import java.time.OffsetDateTime;
+
+object TimeLib {
+
+  // 文字列から修正ユリウス日に変換
+  def stringToModifiedJulianDay(timeStr: String): Double = {
+    val offsetDateTime = OffsetDateTime.parse(timeStr)
+    val second = offsetDateTime.toEpochSecond; // 1970-01-01T00:00:00Zからの秒数
+    second.toDouble / 86400.0 + 40587.0;
+  }
+
+  def modifiedJulianDayToString(time: Double): String = {
+    java.time.Instant.ofEpochSecond(((time - 40587.0) * 86400.0).toLong).toString;
+  }
+
+  def dateStringToDay(str: String): Int = {
+    (stringToModifiedJulianDay(str + "T00:00:00Z") - Main.startTime).toInt;
+  }
+
+  // 2021-04-01
+  def timeToDateString(time: Double): String = {
+    java.time.Instant.ofEpochSecond(((time - 40587.0) * 86400.0 + 0.5).toLong + 9 * 3600).toString.substring(0, 10);
+  }
+
+  // 2021-04-01T12:00
+  def timeToDateTimeString(time: Double): String = {
+    java.time.Instant.ofEpochSecond(((time - 40587.0) * 86400.0 + 0.5).toLong + 9 * 3600).toString.substring(0, 16);
+  }
+
+  // 12時00分
+  def timeToTimeNaturalString(time: Double): String = {
+    val str = java.time.Instant.ofEpochSecond(((time - 40587.0) * 86400.0 + 0.5).toLong + 9 * 3600).toString;
+    "%s時%s分".format(str.substring(11, 13), str.substring(14, 16));
+  }
+
+  def wday(time: Double): Int = {
+    ((time + (9.0 / 24.0 + 0.5 / 86400.0)).toInt + 3) % 7;
+  }
+
+  private def offsetDateTime(time: Double): OffsetDateTime = {
+    java.time.Instant.ofEpochSecond(((time - 40587.0) * 86400.0 + 0.5).toLong).atOffset(java.time.ZoneOffset.ofHours(9));
+  }
+
+  def dayOfMonth(time: Double): Int = {
+    offsetDateTime(time).getDayOfMonth;
+  }
+
+  def dayOfYear(time: Double): Int = {
+    offsetDateTime(time).getDayOfYear;
+  }
+
+  def month(time: Double): Int = {
+    offsetDateTime(time).getMonthValue;
+  }
+
+  def floor(time: Double, stepCountPerDay: Int): Double = {
+    val t1 = time.toInt;
+    val t2 = time - t1;
+    if (stepCountPerDay == 1) {
+      if (t2 < 15.0 / 24) {
+        t1.toDouble - 9.0 / 24;
+      } else {
+        t1.toDouble + 15.0 / 24;
+      }
+    } else {
+      t1 + (t2 * stepCountPerDay).toInt.toDouble / stepCountPerDay;
+    }
+  }
+
+  def round(time: Double, stepCountPerDay: Int): Double = {
+    val t1 = time.toInt;
+    val t2 = time - t1;
+    if (stepCountPerDay == 1) {
+      if (t2 < 15.0 / 24) {
+        t1.toDouble - 9.0 / 24;
+      } else {
+        t1.toDouble + 15.0 / 24;
+      }
+    } else {
+      t1 + (t2 * stepCountPerDay + 0.5).toInt.toDouble / stepCountPerDay;
+    }
+  }
+
+  // "来年1月"
+  def monthString(time0: Double, day1: Int, day2: Int): String = {
+    def sub(time: Double): (Int, Int) = {
+      val str = java.time.Instant.ofEpochSecond(((time - 40587.0) * 86400.0 + 0.5).toLong + 9 * 3600).toString;
+      val year = str.substring(0, 4);
+      val month = str.substring(5, 7);
+      (year.toInt, month.toInt);
+    }
+    val (y0, m0) = sub(time0);
+    val (y1, m1) = sub(Main.startTime + day1);
+    val (y2, m2) = sub(Main.startTime + day2);
+    val s1 = if (y0 == y1 && m0 == m1) {
+      "今月";
+    } else if (y0 == y1 && m0 + 1 == m1 || y0 + 1 == y1 && m0 - 11 == m1) {
+      "来月"
+    } else {
+      (if (y0 == y1) {
+        "";
+      } else if (y0 + 1 == y1) {
+        "来年";
+      } else {
+        "%d年".format(y1);
+      }) + "%d月".format(m1);
+    }
+    if (y1 == y2 && m1 == m2) {
+      s1;
+    } else {
+      s1 + "から" + (if (y0 == y2 || y1 == y2) {
+        "";
+      } else if (y0 + 1 == y2) {
+        "来年";
+      } else {
+        "%d年".format(y2);
+      }) + "%d月".format(m2) + "にかけて";
+    }
+  }
+
+  // UTCからUT1に変換
+  def mjdutcToUt1(time: Double): Double = {
+    time; // 近似的に同一とする
+  }
+
+  // UTCからTDBに変換
+  def mjdutcToTdb(time: Double): Double = {
+    // 近似的
+    //if (time >= 57754.0) {
+      time + (37.0 + 32.184) / 86400.0;
+    //}
+  }
+
+  // UT1から平均恒星時に変換
+  def mjdut1ToGmst(time: Double): Double = {
+    val r = 0.280191 + gmstK * (time - 59215.0);
+    PI2 * (if (r < 0) {
+      r + 1 - r.toInt;
+    } else {
+      r - r.toInt;
+    });
+  }
+
+  def gmstToMjdut1(sid: Double, time0: Double): Double = {
+    val d = MathLib.circleAdd(sid, -mjdut1ToGmst(time0));
+    time0 + d / PI2 / gmstK;
+  }
+
+  private val PI2 = Math.PI * 2.0;
+
+  private val gmstK = 1.0027379094;
+
+}
+
