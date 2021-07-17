41e28d5f594bcf0e5932b55334d86be9f75abc6c
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-07-17 23:59:30 +0900

    星座ツイート修正

--- calc/main.scala
+++ calc/main.scala
@@ -3,9 +3,9 @@ object Main {
 
 // PERIOD
 val startTime = TimeLib.stringToModifiedJulianDay("2021-01-31T00:00:00+09:00");
-val startTime1 = TimeLib.stringToModifiedJulianDay("2021-07-03T00:00:00+09:00");
-val endTime1 = TimeLib.stringToModifiedJulianDay("2022-06-06T00:00:00+09:00");
-val endTime = TimeLib.stringToModifiedJulianDay("2022-06-07T00:00:00+09:00");
+val startTime1 = TimeLib.stringToModifiedJulianDay("2021-07-18T00:00:00+09:00");
+val endTime1 = TimeLib.stringToModifiedJulianDay("2022-06-13T00:00:00+09:00");
+val endTime = TimeLib.stringToModifiedJulianDay("2022-06-13T00:00:00+09:00");
 
 val period = (endTime - startTime).toInt;
 
