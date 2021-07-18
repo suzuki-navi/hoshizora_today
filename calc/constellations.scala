487ef57f6a52a90e59b13978836b4b0c39c290a5
suzuki-navi <suzuki.navi.dev@gmail.com>
2021-07-18 19:22:12 +0900

    refactoring

---
+++ calc/constellations.scala
@@ -0,0 +1,334 @@
+
+object Constellations {
+
+  val PI57 = 180.0 / Math.PI;
+
+  case class Constellation (ra: Double, dec: Double, xyz: Array[Double], name: String, hashtags: List[String],
+    eclipticalFlag: Boolean, galaxyFlag: Boolean);
+
+  def siderealTimeToConstellation(siderealTime: Double, data: IndexedSeq[(String, String)]): String = {
+    val lng5 = (siderealTime * PI57 / 5).toInt * 5;
+    val key = "%02dh%02dm".format(lng5 / 15, lng5 % 15 * 4);
+    val p = data.indexWhere(_._1 > key) - 1;
+    val cons = if (p == -1) {
+      data(data.size - 2)._2;
+    } else if (p >= 0) {
+      data(p)._2;
+    } else {
+      throw new Exception();
+    }
+    cons;
+  }
+
+}
+
+class Constellations(constellationsDataPath: String) {
+
+  val PI57 = 180.0 / Math.PI;
+  val PI2 = Math.PI * 2.0;
+
+  val (culminationContents, lunchTimeContents, lunchTimeContents2, constellationData, starData, starData2, constellationsMap) = {
+    import Ordering.Double.IeeeOrdering;
+
+    def parseContent(content: String): (String, Option[String], List[String], List[String]) = {
+      val UrlPattern = "(.+)\\s(https?://\\S+)".r;
+      val HashtagsPattern = "(.+)\\s+#(\\S+)".r;
+      val StarNamePattern = "(.+)\\s+\\$(\\S+)".r;
+      var content0 = content.trim;
+      var urlOpt: Option[String] = None;
+      var starNames: List[String] = Nil;
+      var hashtags: List[String] = Nil;
+      var f: Boolean = true;
+      while (f) {
+        content0 match {
+          case UrlPattern(c, s) =>
+            content0 = c;
+            urlOpt = Some(s);
+          case HashtagsPattern(c, s) =>
+            content0 = c;
+            hashtags = s :: hashtags;
+          case StarNamePattern(c, s) =>
+            content0 = c;
+            starNames = s :: starNames;
+          case _ =>
+            f = false;
+        }
+      }
+      (content0, urlOpt, hashtags.reverse, starNames.reverse);
+    }
+
+    val source = scala.io.Source.fromFile(constellationsDataPath);
+    var culminationContents: List[(Double, String, Option[String], List[String], List[String])] = Nil;
+    var lunchTimeContents:   List[(Double, String, Option[String], List[String])] = Nil;
+    var lunchTimeContents2:   List[Words.ConstellationLunchTimeContent] = Nil;
+    var constellationData: List[Constellations.Constellation] = Nil;
+    var starData: List[(Double, Array[Double], String, List[String])] = Nil;
+    var starData2: List[(Double, Array[Double], Double, String, List[String])] = Nil;
+    var constellationsMap: Map[String, (String, List[String])] = Map.empty;
+    source.getLines.foreach { line =>
+      if (!line.startsWith("#") && line.length > 7) {
+        val raStr = line.substring(0, 6);
+        val (content0: String, urlOpt: Option[String], hashtags: List[String], starNames: List[String]) = parseContent(line.substring(7));
+        def calcRa(raStr: String): Double = {
+          (raStr.substring(0, 2).toInt.toDouble + raStr.substring(3, 5).toInt.toDouble / 60) / 24 * PI2;
+        }
+        def calcDecXyz(raStr: String, decStr: String): (Double, Double, Array[Double]) = {
+          val ra = calcRa(raStr);
+          val dec = decStr.toDouble / PI57;
+          val z = Math.sin(dec);
+          val xy = Math.cos(dec);
+          val x = xy * Math.cos(ra);
+          val y = xy * Math.sin(ra);
+          val xyz = Array(x, y, z);
+          (ra, dec, xyz);
+        }
+        val ConstellationPattern = "([-+.0-9]+)\\s+Constellation\\s+(Ecliptical\\s+)?(Galaxy\\s+)?(.+)".r;
+        val StarsPattern2 = "([-+.0-9]+)\\s+Stars\\s+Bright\\s+([-+.0-9]+)\\s+(.+)".r;
+        val StarsPattern = "([-+.0-9]+)\\s+Stars\\s+Bright\\s+(.+)".r;
+        val CulminationPattern = "Cul\\s+(.+)".r;
+        val LunchTimeContentPattern = "Lunch\\s+(.+)".r;
+        val LunchTimeContentPattern2 = "([-+.0-9]+)\\s+Lunch\\s+(.+)".r;
+        val AreaPattern = "([-+.0-9]+)\\sArea\\s(.+)".r;
+        content0 match {
+          case ConstellationPattern(decStr, eclipticalStr, galaxyStr, content) =>
+            val (ra, dec, xyz) = calcDecXyz(raStr, decStr);
+            val eclipticalFlag = eclipticalStr != null;
+            val galaxyFlag = galaxyStr != null;
+            constellationData = Constellations.Constellation(ra, dec, xyz, content, hashtags,
+              eclipticalFlag, galaxyFlag) :: constellationData;
+          case StarsPattern2(decStr, magStr, content) =>
+            val (ra, dec, xyz) = calcDecXyz(raStr, decStr);
+            val mag = magStr.toDouble;
+            starData = (ra, xyz, content, hashtags) :: starData;
+            starData2 = (ra, xyz, mag, content, hashtags) :: starData2;
+          case StarsPattern(decStr, content) =>
+            val (ra, dec, xyz) = calcDecXyz(raStr, decStr);
+            starData = (ra, xyz, content, hashtags) :: starData;
+          case CulminationPattern(content) =>
+            val ra = calcRa(raStr);
+            culminationContents = (ra, content, urlOpt, hashtags, starNames) :: culminationContents;
+          case LunchTimeContentPattern(content) =>
+            val ra = calcRa(raStr);
+            lunchTimeContents = (ra, content, urlOpt, hashtags) :: lunchTimeContents;
+          case LunchTimeContentPattern2(decStr, content) =>
+            val (ra, dec, xyz) = calcDecXyz(raStr, decStr);
+            val p = content.indexOf("座");
+            val word = content.substring(0, p + 1);
+            val hashtags2 = hashtags ::: word :: "星座" :: Nil;
+            lunchTimeContents2 = Words.ConstellationLunchTimeContent(word, content, urlOpt, hashtags2, ra, dec, xyz) :: lunchTimeContents2;
+          case AreaPattern(decStr, content) =>
+            val lngH = raStr.substring(0, 2).toInt;
+            val lngM = raStr.substring(3, 5).toInt / 20 * 20;
+            val lat5 = decStr.toInt;
+            val key = "%2dh%02dm,%3d".format(lngH, lngM, lat5);
+            constellationsMap = constellationsMap ++ Map(key -> (content, hashtags));
+          case _ => // nothing
+        }
+      }
+    }
+    source.close();
+    (
+      culminationContents.reverse.toIndexedSeq.sortBy(_._1),
+      lunchTimeContents.reverse.toIndexedSeq.sortBy(_._1),
+      lunchTimeContents2.reverse.toIndexedSeq.sortBy(_.ra),
+      constellationData.reverse.toIndexedSeq.sortBy(_.ra),
+      starData.reverse.toIndexedSeq.sortBy(_._1),
+      starData2.reverse.toIndexedSeq.sortBy(_._1),
+      constellationsMap,
+    );
+  }
+
+  private def icrsToConstellation(lng: Double, lat: Double): (String, List[String]) = {
+    val lng5 = (lng * PI57 / 5).toInt * 5;
+    val lat5 = ((lat * PI57 + 90) / 5).toInt * 5 - 90;
+    val key = "%2dh%02dm,%3d".format(lng5 / 15, lng5 % 15 * 4, lat5);
+    val (cons, hashtags) = constellationsMap.getOrElse(key, ("", Nil));
+    if (cons == "") {
+      ("(ERROR %s)".format(key), hashtags);
+    } else {
+      (cons, hashtags);
+    }
+  }
+
+  def icrsToConstellation(xyz: Array[Double]): (String, List[String]) = {
+    val lng = VectorLib.xyzToLng(xyz);
+    val lat = VectorLib.xyzToLat(xyz);
+    icrsToConstellation(lng, lat);
+  }
+
+}
+
+object Words {
+
+  val PI57 = 180.0 / Math.PI;
+
+  trait LunchTimeContent {
+    def word: String;
+    def content(day: Int, bpn: Bpn, hcs: Hcs): String;
+    def urlOpt: Option[String];
+    def hashtags: List[String];
+  }
+
+  case class ConstellationLunchTimeContent (word: String, contentTemplate: String,
+    urlOpt: Option[String], hashtags: List[String],
+    ra: Double, dec: Double, xyz: Array[Double]) extends LunchTimeContent {
+    def content(day: Int, bpn: Bpn, hcs: Hcs): String = {
+      val altHor = -0.90 / PI57;
+      val time = Main.startTime + day + 21.0 / 24;
+      val tdb = TimeLib.mjdutcToTdb(time);
+      val bpnMatrix = bpn.icrsToTrueEquatorialMatrix(tdb);
+      val xyz2 = VectorLib.multiplyMV(bpnMatrix, xyz);
+      val (azi, alt) = hcs.trueEquatorialXyzToAziAltFromUtc(xyz2, time);
+      if (alt < altHor) {
+        "";
+      } else {
+        val hcsStr = Hcs.aziAltToNaturalString(azi, alt);
+        replaceSouthPattern(contentTemplate, hcsStr);
+      }
+    }
+
+    private[this] def replaceSouthPattern(template: String, hcsStr: String): String = {
+      val template2 = if (hcsStr.startsWith("南の") || hcsStr.startsWith("北の空高く")) {
+        template.replace("[", "").replace("]", "");
+      } else {
+        var f: Boolean = true;
+        var template2 = template;
+        while (f) {
+          f = false;
+          val p1 = template2.indexOf("[");
+          if (p1 >= 0) {
+            val p2 = template2.indexOf("]", p1);
+            if (p2 > p1) {
+              template2 = template2.substring(0, p1) + template2.substring(p2 + 1);
+              f = true;
+            }
+          }
+        }
+        template2;
+      }
+      if (template2.indexOf("%") >= 0) {
+        template2.replace("%", hcsStr);
+      } else {
+        template2;
+      }
+    }
+  }
+
+}
+
+class Words(constellationData: Constellations) {
+
+  private def fetchLunchTimeContents(word: String): IndexedSeq[Words.LunchTimeContent] = {
+    constellationData.lunchTimeContents2.filter(_.word == word);
+  }
+
+  private val defaultLimit = 25;
+  private var history: Map[String, (List[Int], Int)] = Map.empty;
+  private var contents: Map[String, IndexedSeq[Words.LunchTimeContent]] = Map.empty;
+
+  val periodStart: Int = 168; // PERIOD 2021/07/18
+
+  def loadHistory(path: String): Unit = {
+    history = Map.empty;
+    val source = scala.io.Source.fromFile(path);
+    source.getLines.foreach { line =>
+      val cols = line.split(":");
+      val word = cols(0);
+      val (days, limit) = history.getOrElse(word, (Nil, defaultLimit));
+      if (cols(1).startsWith("limit ")) {
+        val limit = cols(1).substring(6).toInt;
+        history = history + (word -> (days, limit));
+      } else {
+        val day = TimeLib.dateStringToDay(cols(1));
+        if (day < periodStart) {
+          history = history + (word -> (day :: days, limit));
+        }
+      }
+    }
+    source.close();
+  }
+
+  def saveHistory(path: String): Unit = {
+    scala.util.Using(new java.io.PrintWriter(new java.io.FileOutputStream(path))) { writer =>
+      history.keySet.toSeq.sorted.foreach { word =>
+        val (days, limit) = history(word);
+        {
+          val line = "%s:limit %d".format(word, limit);
+          writer.println(line);
+        }
+        days.reverse.foreach { day =>
+          val line = "%s:%s".format(word, TimeLib.timeToDateString(Main.startTime + day));
+          writer.println(line);
+        }
+      }
+    }
+  }
+
+  private def tweetPoint(word: String, day: Int): (IndexedSeq[Words.LunchTimeContent], Double) = {
+    val cs = contents.getOrElse(word, {
+      val cs = fetchLunchTimeContents(word);
+      contents = contents + (word -> cs);
+      cs;
+    });
+    if (cs.isEmpty) {
+      (IndexedSeq.empty, 0.0);
+    } else {
+      val (days, limit) = history.getOrElse(word, (Nil, defaultLimit));
+      if (days.isEmpty) {
+        (cs, 10000.0);
+      } else {
+        val d = day - days.head;
+        if (d < limit) {
+          (cs, 0.0);
+        } else {
+          (cs, d.toDouble / limit);
+        }
+      }
+    }
+  }
+
+  private def findRecentWord(day: Int, bpn: Bpn, hcs: Hcs): Option[(String, IndexedSeq[Words.LunchTimeContent])] = {
+    val time = Main.startTime + day + 12.0 / 24;
+    val lastTweets = (-13 to 0).flatMap(i => Main.getTweets(Main.startTime + day + i).tweets);
+    val lst = lastTweets.flatMap(_.starNames);
+    if (lst.nonEmpty) {
+      import Ordering.Double.IeeeOrdering;
+      val lst2 = lst.map { w => (w, tweetPoint(w, day)) }.sortBy(- _._2._2);
+      lst2.find { case (word, (contents, point)) =>
+        point >= 1.0 && contents.filter(_.content(day, bpn, hcs) != "").nonEmpty;
+      } match {
+        case Some((word, (contents, point))) => Some((word, contents));
+        case _ => None;
+      }
+    } else {
+      None;
+    }
+  }
+
+  private def addHistory(word: String, day: Int): Unit = {
+    val (days, limit) = history.getOrElse(word, (Nil, defaultLimit));
+    history = history + (word -> (day :: days, limit));
+  }
+
+  def putTweetWord(day: Int, bpn: Bpn, hcs: Hcs): Unit = {
+    findRecentWord(day, bpn, hcs) match {
+      case None => ;
+      case Some((word, contents)) =>
+        val time1 = Main.startTime + day + (12.0 + 50.0 / 60) / 24;
+        var inc: Int = 0;
+        contents.foreach { content =>
+          val c = content.content(day + inc / 4, bpn, hcs);
+          if (c != "") {
+            val msg = c + content.hashtags.map(" #" + _).mkString("");
+            Main.putTweet(time1 + 1.0 * (inc % 4) / 24, msg, content.urlOpt);
+            if (inc == 0) {
+              addHistory(word, day + inc / 4);
+            }
+            inc += 1;
+          }
+        }
+    }
+  }
+
+}
+
---
+++ calc/hcs.scala
@@ -0,0 +1,126 @@
+
+class Hcs(lng: Double, lat: Double) {
+
+  val PI2 = Math.PI * 2.0;
+  val PI5 = Math.PI * 0.5;
+
+  def siderealTimeFromUtc(utc: Double): Double = {
+    val ut1 = utc; // 近似的
+    siderealTimeFromUt1(ut1);
+  }
+
+  def siderealTimeFromUt1(ut1: Double): Double = {
+    val gmst = TimeLib.mjdut1ToGmst(ut1);
+    val gast = gmst; // 近似
+    val s = gast + lng;
+    if (s >= PI2) {
+      s - PI2;
+    } else {
+      s;
+    }
+  }
+
+  def siderealTimeToUtc(sid: Double, time0: Double): Double = {
+    siderealTimeToUt1(sid, time0); // 近似
+  }
+
+  def siderealTimeToUt1(sid: Double, time0: Double): Double = {
+    val s = sid - lng;
+    val s2 = if (s < 0) {
+      s + PI2;
+    } else {
+      s;
+    }
+    TimeLib.gmstToMjdut1(s2, time0);
+  }
+
+  def trueEquatorialXyzToAziAltFromUtc(xyz: Array[Double], utc: Double): (Double, Double) = {
+    val ut1 = utc; // 近似的
+    trueEquatorialXyzToAziAltFromUt1(xyz, ut1);
+  }
+
+  def trueEquatorialXyzToAziAltFromUt1(xyz: Array[Double], ut1: Double): (Double, Double) = {
+    var r: Array[Double] = VectorLib.unitMatrix;
+    r = VectorLib.rotateMatrixZ(PI5 - siderealTimeFromUt1(ut1), r);
+    r = VectorLib.rotateMatrixX(PI5 - lat, r);
+    // 地平座標系 X:西 Y:南 Z:天頂
+    val hcs = VectorLib.multiplyMV(r, xyz);
+    val x = hcs(0);
+    val y = hcs(1);
+    val z = hcs(2);
+    val azi1 = Math.atan2(-x, -y);
+    val azi = if (azi1 < 0) azi1 + PI2 else azi1; // 北0°、東90°、南180°、西270°
+    val xy = Math.sqrt(x * x + y * y);
+    val alt = Math.atan2(z, xy);
+    (azi, alt);
+  }
+
+}
+
+object Hcs {
+
+  val PI57 = 180.0 / Math.PI;
+
+  def aziAltToNaturalString(azi: Double, alt: Double): String = {
+    val altHor = -0.90 / PI57;
+    val alt360 = alt * PI57;
+    if (alt360 >= 80) {
+      "天頂付近";
+    } else {
+      val azi360 = azi * PI57;
+      val s1 = if (azi360 <= 22.5 || azi360 >= 337.5) {
+        "北の";
+      } else if (azi360 < 67.5) {
+        "北東の";
+      } else if (azi360 > 292.5) {
+        "北西の";
+      } else if (azi360 <= 112.5) {
+        "東の";
+      } else if (azi360 >= 247.5) {
+        "西の";
+      } else if (azi360 < 157.5) {
+        "南東の";
+      } else if (azi360 > 202.5) {
+        "南西の";
+      } else {
+        "南の";
+      }
+      val s2 = if (alt < altHor) {
+        "地平線の下";
+      } else if (alt360 < 10.0) {
+        "空地平線近く";
+      } else if (alt360 < 30.0) {
+        "空低く";
+      } else if (alt360 >= 60.0) {
+        "空高く";
+      } else {
+        "空";
+      }
+      s1 + s2;
+    }
+  }
+
+  def aziAltToNaturalStringC(azi: Double, alt: Double): (String, Double) = {
+    val alt360 = alt * PI57;
+    if (alt360 >= 80) {
+      ("天頂", 0.0);
+    } else if (alt360 < 30) {
+      ("", 0.0);
+    } else {
+      val azi360 = azi * PI57;
+      if (azi360 <= 45) {
+        ("北の空", -alt);
+      } else if (azi360 >= 315) {
+        ("北の空", -alt);
+      } else if (azi360 <= 135) {
+        ("東の空", -alt);
+      } else if (azi360 >= 225) {
+        ("西の空", -alt);
+      } else {
+        ("南の空", -alt);
+      }
+    }
+  }
+
+}
+
